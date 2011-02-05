73dc5ddca21aa1d3fbd8088580b20f90c2bdde30
Ben Langfeld <ben@langfeld.me>
2011-02-05 12:39:30 +0100

    Base Rails app (with nifty generators)

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+KeplerProcessorApp::Application.config.secret_token = '4e7257d6372d29370cf7e2eb1e88b18a478225bc83954e4bc1c11583c29a22a2f0949ed10bad1ae57840e82fdd7034be0f03455d7ff9908ed59d945b2935d486'
